f5ce2cc03a47957467953c43a8afe7a5b2f7d0ef
Filip Trbusek <filiptrbusek@gmail.com>
2021-12-27 15:32:47 +0100

    list items and keys added

--- grocery-list-app/src/Components/Content.js
+++ grocery-list-app/src/Components/Content.js
@@ -1,12 +1,35 @@
+import { useState } from "react";
+
 const Content = () => {
-  const handleNameChange = () => {
-    const names = ["Filip", "John", "Mark"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  };
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
+    },
+    {
+      id: 1,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 1,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
+
   return (
     <main>
-      <p>Hello {handleNameChange()}</p>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input type="checkbox" checked={item.checked} />
+            <label>{item.item}</label>
+            <button>Delete</button>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
